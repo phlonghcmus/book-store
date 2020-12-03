f27b5a9772e9788bf9f0d60aaa92b4b550595d45
DucLoc <ducloc0009@gmail.com>
2020-12-03 13:09:14 +0700

    UPDATE PAGINATION + ADD SEARCH FEATURE + SEARCH PAGINATION

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -1,27 +1,53 @@
 const bookModel = require('../models/bookModel');
+const toQS = require('querystring').stringify;
+
 
 exports.list = async(req, res, next) => {
-    const currentPage=req.query.p || 1;
-    console.log(currentPage);
-    // Get books from model
-    let books = await bookModel.list();
-    let pageCount=books.length/6;
-    const intPageCount=pageCount;
-    if(pageCount>parseInt(String(intPageCount)))
-        Math.ceil(pageCount);
-    console.log(Math.ceil(pageCount));
-    books=await bookModel.listPerPage(currentPage);
+    let currentPage=req.query.p || 1;
+    let keyword=req.query.keyword;
+    let books;
+    let pageCount;
+    let newQuery;
+    if(keyword)
+    {
+        console.log(keyword);
+        pageCount= await bookModel.pageCountSearch(keyword);
+        books=await bookModel.searchPerPage(keyword,currentPage);
+        const query=req.query;
+        newQuery=toQS(query);
+        if(req.query.p)
+        {
+            newQuery=newQuery.split('&').shift();
+        }
+        newQuery+="&";
+    }
+    else {
+        // Get books from model
+        pageCount = await bookModel.pageCountList();
+        console.log(pageCount);
+        books=await bookModel.listPerPage(currentPage);const query=req.query;
+    }
+    if(currentPage>Math.ceil(pageCount))
+    {
+        req.query.p= 5 ||1;
+    }
+    const count=books.length;
     // Pass data to view to display list of books
-    res.render('books/list', {books,pagination:{page:currentPage,pageCount:Math.ceil(pageCount)}});
+    if(keyword)
+        res.render('books/list', {books,count,query:newQuery,pagination:{page:currentPage,pageCount:Math.ceil(pageCount)}});
+    else
+        res.render('books/list', {books,count,pagination:{page:currentPage,pageCount:Math.ceil(pageCount)}});
 };
+
+
 
 exports.detail=async (req, res, next) =>
 {
-    const books = await bookModel.list();
+   
     const tt= await bookModel.get(req.params.id);
     const title=tt.title;
     const basePrice=tt.basePrice;
     const detail=tt.detail;
     const cover=tt.cover;
-    res.render('books/detail',{title,basePrice,detail,cover,books});
+    res.render('books/detail',{title,basePrice,detail,cover});
 };
